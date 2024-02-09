7e598c0a560ee5fbab14f4945220012280c1e5ba
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-02-09 16:35:20 +0530

    React/Vite

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className="footer">
+    copyright Raunak.com | All Rights  reserved
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+
+
+import "./NavBar.css"
+const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+    </div>
+
+  )
+}
+
+export default NavBar
